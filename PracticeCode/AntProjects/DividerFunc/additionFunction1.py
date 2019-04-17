0b195eac906aec7c5666b74c94e8ebbab078f1a5
Raul Veras <verasraul@gmail.com>
2019-04-16 20:30:12 -0400

    Fixed Doctstrings in Addition Func and added Test for addition Module

--- PracticeCode/AntProjects/DividerFunc/additionFunction1.py
+++ PracticeCode/AntProjects/DividerFunc/additionFunction1.py
@@ -3,15 +3,15 @@ __author__ = 'verasraul'
 '''
 parameters = number1, number2
 accepted type = both parameters accept floats and ints
-description = divide number1 by number2
-return type(s) = returns a float type
+description = add number1 by number2
+return type(s) = integer (if added with ints) & floats (if added with floats)
 
 
 '''
 
 
 def addition (number1, number2):
-    # Func takes 2 ints as arguments.
+    # Func takes 2 ints and/or floats as arguments.
     # Return the result of number1 + number2.
     return (number1 + number2)
 
