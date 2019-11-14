2207ed525590294697a41c9bfccbdca7da689c17
Raul Veras <verasraul@gmail.com>
2019-11-13 21:13:17 -0500

    Edited documentation to reflect the code changes for each function.

--- PracticeCode/AntProjects/MiniCalc/additionFunction1.py
+++ PracticeCode/AntProjects/MiniCalc/additionFunction1.py
@@ -3,7 +3,7 @@ __author__ = 'verasraul'
 '''parameters = number1, number2
 accepted type = both parameters accept floats and ints
 description = add number1 by number2
-return type(s) = integer (if added with ints) & floats (if added with floats)
+return type(s) = float
 '''
 
 
--- PracticeCode/AntProjects/MiniCalc/dividerFunction1.py
+++ PracticeCode/AntProjects/MiniCalc/dividerFunction1.py
@@ -3,7 +3,7 @@ __author__ = 'verasraul'
 '''parameters = number1, number2
 accepted type = both parameters accept floats and ints
 description = divide number1 by number2
-return type(s) = returns a float type
+return type(s) = float
 '''
 
 
