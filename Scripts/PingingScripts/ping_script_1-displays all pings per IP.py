9d90bcdb6e0acf57ca28e89d679201d5aebd9fe7
Raul Veras <verasraul@gmail.com>
2019-11-14 14:17:31 -0500

    Added 'PingingScripts' folder.

---
+++ Scripts/PingingScripts/ping_script_1-displays all pings per IP.py
@@ -0,0 +1,19 @@
+#removed os lib import
+import subprocess
+
+ips = {'Corporate':'10.10.1.1', 'Madison': '10.10.21.1',
+       'Costa Mesa': '10.10.23.1', 'Melrose': '10.10.24.1',
+       'Bal Harbor': '10.10.27.1', 'Elmhurst': '10.10.28.1'}
+
+def ping_stores():
+    for name, value in ips.items():
+        process = subprocess.call(['ping', '-n', '5', value])
+        if process == 0:
+            print ("ping to", name, "OK")
+        elif process == 2:
+            print ("no response from", name)
+        else:
+            print ("ping to", name, "failed!")
+            
+stores = ping_stores()
+stores
