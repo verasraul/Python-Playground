15f414f40ce9edc7cfe9dee44a5fd1f0bfb078f2
Raul Veras <verasraul@gmail.com>
2019-11-13 21:12:12 -0500

    Added divider function.

---
+++ PracticeCode/AntProjects/MiniCalc/subtractionFunction1.py
@@ -0,0 +1,13 @@
+__author__ = 'verasraul'
+
+'''parameters = number1, number2
+accepted type = both parameters accept floats and ints
+description = subtract number1 by number2
+return type(s) = float
+'''
+
+
+def subtraction (number1, number2):
+    # Takes 2 ints and/or floats as arguments.
+    # Returns the float result of number1 - number2.
+    return (float(number1) - float(number2))
